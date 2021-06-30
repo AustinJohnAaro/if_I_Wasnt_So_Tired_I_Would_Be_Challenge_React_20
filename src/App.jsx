200a9ee5e19705014c8e105b5c80bfdeccb45037
AustinJohnAaro <austin1472a@gmail.com>
2021-06-29 23:47:44 -0700

    just for a test

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import {resolveModuleName} from 'ts-pnp';
+import * as ts from 'typescript';
+
+
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
@@ -6,6 +10,55 @@ import ContactForm from './components/Contact';
 
 import Select from 'react-select';
 
+function App() {
+    const [categories] = useState([
+      {
+        name: 'commercial',
+        description: 'Photos of grocery stores, food trucks, and other commercial projects',
+      },
+      { name: 'portraits', description: 'Portraits of people in my life' },
+      { name: 'food', description: 'Delicious delicacies' },
+      { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    ]);
+  
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  
+    const [contactSelected, setContactSelected] = useState(false);
+  
+    return (
+      <div>
+        <Nav
+          categories={categories}
+          setCurrentCategory={setCurrentCategory}
+          currentCategory={currentCategory}
+          contactSelected={contactSelected}
+          setContactSelected={setContactSelected}
+        ></Nav>
+        <main>
+          {!contactSelected ? (
+            <>
+              <Gallery currentCategory={currentCategory}></Gallery>
+              <About></About>
+            </>
+          ) : (
+            <ContactForm></ContactForm>
+          )}
+        </main>
+      </div>
+    );
+  }
+  
+  
 
 
-export default App;
+
+  
+
+  
+
+  
+
+
+
+
+export default App;  
